750748f6888c83fd4053a2a777ab3bdcc36f087a
Jorge Sanchez <jorgeasr1993@gmail.com>
2024-06-16 19:37:31 -0500

    new

---
+++ ExamenFinal/final_exam/src/test/java/com/exam/final_exam/FinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.final_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
